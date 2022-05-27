b7490b9443565dcfac5d5d565f279dc1036e29de
Lucas Emanuel Souza Silva <lucasgitacc@gmail.com>
2022-05-27 00:34:41 -0300

    Parte da documentação feita

--- assets/js/discussion.js
+++ assets/js/discussion.js
@@ -1,6 +1,9 @@
+//Aqui é onde acontece toda a magia do código. Como não há persistencia, devemos ter uma maneira de anotar o id de cada tópico. O errorCounter serve para não apresentar a mensagem de erro mais de uma vez
+//o counter serve como o id atual
 let counter = 0
 let errorCounter = 0
 
+//Esta função tira todas as childs do discussion-header, após isso copia o template que trata do formulário de envio de questão, e adiciona ele ao header
 function discussionText(){
     const header = document.querySelector('#discussion-header');
     while(header.firstChild){
@@ -15,6 +18,13 @@ function discussionText(){
     header.appendChild(formClon)
 }
 
+
+//Esta função faz literalmente toda a magia do projeto.
+//Ela pega o nome, o assunto e o conteúdo da questão, chega se são vazios, para caso sejam, adicionar uma mensagem de erro, e após isso, ela pega o template-1, que trata do design das questões
+//coloca um id no tópico, cria o nome do autor no tópico, adiciona o assunto, depois o conteúdo.
+//Depois de tudo isso, ela cria o botão de dropdown com o id referente ao id do post, e com a função de enviar resposta
+//Adiciona a função ao contador de likes
+//adiciona a função ao contador de respostas
 function topicSent(){
     let name = document.getElementById('name');
     let subject = document.getElementById('subject');
@@ -83,6 +93,7 @@ function topicSent(){
 
 }
 
+//Remove o formulário ao envio da questão e adiciona a mensagem de sucesso
 function topicSuccess(){
     let lastTemplate = document.querySelector('#template-3')
     let lastTemaplateClone = lastTemplate.content.cloneNode(true)
@@ -94,18 +105,19 @@ function topicSuccess(){
 
     header.appendChild(lastTemaplateClone)
 }
-
+//faz a contagem do likes pegando a div pai pelo id, fazendo uma query dentro dela procurando o span que tem a quantidade de likes, e adicionando 1, isto é pela falta de persistência
 function like(id){
     let element = document.querySelector(`#topic-${id}`)
     let likeSpan = element.querySelector('[name="numLikes"]')
     likeSpan.textContent = eval(likeSpan.textContent) + 1
 }
+//faz a mesma coisa que o contador de Id, só que para as respostas
 function answerCounter(id){
     let element = document.querySelector(`#${id}`)
     let answSpan = element.querySelector('[name="numAnsw"]')
     answSpan.textContent = eval(answSpan.textContent) + 1
 }
-
+//Cria a resposta abaixo da div de tópico. Vale salientar que o formulário de tópico é a modal
 function answerSent(id){
     let answerName = document.getElementById('answer-name');
     let answerContent = document.getElementById('answer-content');
@@ -138,7 +150,7 @@ function answerSent(id){
     parentNode.appendChild(answerTemplateClone)
     answerCounter(id)
 }
-
+//Aqui é a engrenagem do sistema, ela coloca o id na função do modal pra a função saber em qual div adicionar as respostas
 function passId(id){
     let answerButton = document.querySelector('#sendAnswerButton');
     console.log(answerButton)
